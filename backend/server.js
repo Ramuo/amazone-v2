a204b59fbe490b0d0311ce72e7ec09a5842571a3
Barry <barry.alpha88@outlook.fr>
2023-07-15 15:29:59 +0200

    final

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/*
 import path from 'path';
 import express from "express";
 import cookieParser from 'cookie-parser';
@@ -61,6 +62,74 @@ app.use(errorHandler);
 
 //SERVER
 app.listen(PORT, () => console.log(`Server running ont ${PORT}`));
+*/
+
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+
+
+const port = process.env.PORT || 5000;
+
+//CONNECT TO MongoDB
+connectDB();
+
+//INITIALIZE EXPRESS
+const app = express();
+
+// Bordy parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Cookie parsr middleware
+app.use(cookieParser());
+
+
+//ROUTES
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+//PAYPAL ROUTE
+app.get('/api/config/paypal', (req, res) => res.send({clientId: 
+process.env.PAYPAL_CLIENT_ID}));
+
+
+//STATIC ROUTE
+const __dirname = path.resolve(); //Set __dir to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    //any route that is not api will redirect to index.html
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+}else{
+    app.get('/', (req, res) => res.send('API Running'));
+};
+
+
+//MIDDLEWARE
+app.use(notFound);
+app.use(errorHandler);
+
+//SERVER
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
 
 
